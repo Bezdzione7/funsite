e3dbd3ebf8453ecf5bcd44e38a2c6672b438d1c2
taute.skritulskas <taute.skritulskas@gmail.com>
2025-10-26 02:31:54 +0300

    Remove Vercel configs and prepare for Railway deployment

--- rugplay-main/website/svelte.config.js
+++ rugplay-main/website/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,10 +9,7 @@ const config = {
 
 	kit: {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			runtime: 'nodejs18.x',
-			regions: ['iad1']
-		})
+		adapter: adapter()
 	}
 };
 
